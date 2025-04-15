17c7fcf94922ac8053202485af581595d19966a3
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-15 17:35:53 -0300

    Building the Event Form Component

--- convex/storage.js
+++ convex/storage.js
@@ -1,22 +1,21 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
-// import { mutation, query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
-// export const generateUploadUrl = mutation(async (ctx) => {
-//   return await ctx.storage.generateUploadUrl();
-// });
+export const generateUploadUrl = mutation(async (ctx) => {
+  return await ctx.storage.generateUploadUrl();
+});
 
-// export const updateEventImage = mutation({
-//   args: {
-//     eventId: v.id("events"),
-//     storageId: v.union(v.id("_storage"), v.null()),
-//   },
-//   handler: async (ctx, { eventId, storageId }) => {
-//     await ctx.db.patch(eventId, {
-//       imageStorageId: storageId ?? undefined,
-//     });
-//   },
-// });
+export const updateEventImage = mutation({
+  args: {
+    eventId: v.id("events"),
+    storageId: v.union(v.id("_storage"), v.null()),
+  },
+  handler: async (ctx, { eventId, storageId }) => {
+    await ctx.db.patch(eventId, {
+      imageStorageId: storageId ?? undefined,
+    });
+  },
+});
 
 export const getUrl = query({
   args: { storageId: v.id("_storage") },
@@ -25,9 +24,9 @@ export const getUrl = query({
   },
 });
 
-// export const deleteImage = mutation({
-//   args: { storageId: v.id("_storage") },
-//   handler: async (ctx, { storageId }) => {
-//     await ctx.storage.delete(storageId);
-//   },
-// });
+export const deleteImage = mutation({
+  args: { storageId: v.id("_storage") },
+  handler: async (ctx, { storageId }) => {
+    await ctx.storage.delete(storageId);
+  },
+});
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      { hostname: "accurate-parrot-654.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
