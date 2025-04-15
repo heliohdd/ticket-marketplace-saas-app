e0963492e4444da929a85553bab9fb44639f0d82
Helio Dourado <heliohdd@yahoo.com.br>
2025-04-15 20:18:50 -0300

    Completing Stripe Connect Implementation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
+  // eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { hostname: "accurate-parrot-654.convex.cloud", protocol: "https" },
